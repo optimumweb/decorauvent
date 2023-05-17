ecaa930c83621dc54a69881243089e45982dbc94
Jonathan Roy <jroy66@gmail.com>
2023-05-17 13:57:30 -0400

    append file names

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -4,4 +4,25 @@ $(document).ready(function () {
     });
 
     $('.stonehenge').stonehenge();
+
+    $('.file').each(function () {
+        let $file = $(this),
+            $fileInput = $file.find('.file-input'),
+            $fileName = $file.find('.file-name'),
+            files,
+            fileNames;
+
+        $fileInput.on('change', function (e) {
+            files = e.target.files;
+            fileNames = [];
+
+            if (files.length > 0) {
+                for (let i = 0; i < files.length; i++) {
+                    fileNames.push(files[i].name);
+                }
+            }
+
+            $fileName.text(fileNames.join(', '));
+        });
+    });
 });
