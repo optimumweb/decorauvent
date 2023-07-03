b16c62b79a8a9c14b008041c56ccc3a604ffc015
Jonathan Roy <jroy66@gmail.com>
2023-07-03 08:14:18 -0400

    style

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,15 +1,10 @@
-const $window = $(window);
-const $document = $(document);
-
-$document.ready(function () {
+$(document).ready(function () {
     AOS.init({
         duration: 800
     });
 
-    let $body = $('body'),
-        $siteHeader = $('#site-header');
-
-    $body.css('padding-top', $siteHeader.outerHeight(true));
+    const $window = $(window);
+    const $body = $('body');
 
     $window.on('scroll', () => {
         if ($window.scrollTop() > 100) {
