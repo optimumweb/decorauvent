11a9c68b401c8ac6d8e4b3fb4df532e05a640e4a
Jonathan Roy <jroy66@gmail.com>
2023-07-13 18:07:57 -0400

    capture invalid recaptcha

--- boot.php
+++ boot.php
@@ -17,121 +17,127 @@ if (isset($requestData['google_recaptcha_token'])) {
 }
 
 if (isset($requestData['free_estimate'])) {
-    $freeEstimateData = $requestData['free_estimate'];
+    try {
+        $freeEstimateData = $requestData['free_estimate'];
 
-    if (isset($googleRecaptchaAssessment) && $googleRecaptchaAssessment === true) {
-        try {
-            $freeEstimateNotify = $site->theme()->setting('free_estimate_notify');
+        if (isset($googleRecaptchaAssessment) && $googleRecaptchaAssessment === true) {
+            try {
+                $freeEstimateNotify = $site->theme()->setting('free_estimate_notify');
 
-            if (isset($freeEstimateNotify)) {
-                $freeEstimateNotify = array_map('trim', explode(',', $freeEstimateNotify));
-                $freeEstimateCc = array_map('trim', explode(',', $site->theme()->setting('free_estimate_cc')));
+                if (isset($freeEstimateNotify)) {
+                    $freeEstimateNotify = array_map('trim', explode(',', $freeEstimateNotify));
+                    $freeEstimateCc = array_map('trim', explode(',', $site->theme()->setting('free_estimate_cc')));
 
-                \Illuminate\Support\Facades\Mail::to($freeEstimateNotify)
-                    ->cc($freeEstimateCc)
-                    ->send(new \Theme\Mailables\FreeEstimateRequest($freeEstimateData));
-            } else {
-                throw new \Exception("Setting 'free_estimate_notify' not defined");
+                    \Illuminate\Support\Facades\Mail::to($freeEstimateNotify)
+                        ->cc($freeEstimateCc)
+                        ->send(new \Theme\Mailables\FreeEstimateRequest($freeEstimateData));
+                } else {
+                    throw new \Exception("Setting 'free_estimate_notify' not defined");
+                }
+            } catch (\Throwable $e) {
+                \Sentry\captureException($e);
             }
-        } catch (\Throwable $e) {
-            \Sentry\captureException($e);
-        }
 
-        try {
-            $pipedriveApiToken = $site->theme()->setting('pipedrive_api_token');
-            $pipedrivePostalAddressFieldToken = $site->theme()->setting('pipedrive_postal_address_field_token');
+            try {
+                $pipedriveApiToken = $site->theme()->setting('pipedrive_api_token');
+                $pipedrivePostalAddressFieldToken = $site->theme()->setting('pipedrive_postal_address_field_token');
 
-            if (isset($pipedriveApiToken)) {
-                $pipedriveClient = new \Pipedrive\Client(
-                    apiToken: $pipedriveApiToken,
-                );
+                if (isset($pipedriveApiToken)) {
+                    $pipedriveClient = new \Pipedrive\Client(
+                        apiToken: $pipedriveApiToken,
+                    );
 
-                $pipedrivePerson = [
-                    'name' => $freeEstimateData['name']['value'] ?? '???',
-                    'visible_to' => 3, // 3 = entire company
-                ];
-
-                if (isset($pipedrivePostalAddressFieldToken)) {
-                    $pipedrivePerson[$pipedrivePostalAddressFieldToken] = "{$freeEstimateData['city']['value']}, {$freeEstimateData['postal_code']['value']}";
-                }
-
-                if (isset($freeEstimateData['tel']['value'])) {
-                    $pipedrivePerson['phone'] = [
-                        'value' => $freeEstimateData['tel']['value'],
-                        'primary' => true,
-                        'label' => "form",
-                    ];
-                }
-
-                if (isset($freeEstimateData['email']['value'])) {
-                    $pipedrivePerson['email'] = [
-                        'value' => $freeEstimateData['email']['value'],
-                        'primary' => true,
-                        'label' => "form",
-                    ];
-                }
-
-                $addAPersonResponse = $pipedriveClient->getPersons()->addAPerson($pipedrivePerson);
-
-                if ($addAPersonResponse->success && isset($addAPersonResponse->data->id)) {
-                    $pipedrivePersonId = $addAPersonResponse->data->id;
-
-                    $pipedriveDeal = [
-                        'title' => $freeEstimateData['name']['value'],
-                        'person_id' => $pipedrivePersonId,
+                    $pipedrivePerson = [
+                        'name' => $freeEstimateData['name']['value'] ?? '???',
+                        'visible_to' => 3, // 3 = entire company
                     ];
 
-                    $addADealResponse = $pipedriveClient->getDeals()->addADeal($pipedriveDeal);
+                    if (isset($pipedrivePostalAddressFieldToken)) {
+                        $pipedrivePerson[$pipedrivePostalAddressFieldToken] = "{$freeEstimateData['city']['value']}, {$freeEstimateData['postal_code']['value']}";
+                    }
 
-                    if ($addADealResponse->success && isset($addADealResponse->data->id)) {
-                        $pipedriveDealId = $addADealResponse->data->id;
+                    if (isset($freeEstimateData['tel']['value'])) {
+                        $pipedrivePerson['phone'] = [
+                            'value' => $freeEstimateData['tel']['value'],
+                            'primary' => true,
+                            'label' => "form",
+                        ];
+                    }
 
-                        if (isset($freeEstimateData['comments']['value'])) {
-                            $pipedriveNote = [
-                                'content' => $freeEstimateData['comments']['value'],
-                                'dealId' => $pipedriveDealId,
-                            ];
+                    if (isset($freeEstimateData['email']['value'])) {
+                        $pipedrivePerson['email'] = [
+                            'value' => $freeEstimateData['email']['value'],
+                            'primary' => true,
+                            'label' => "form",
+                        ];
+                    }
 
-                            $addANoteResponse = $pipedriveClient->getNotes()->addANote($pipedriveNote);
+                    $addAPersonResponse = $pipedriveClient->getPersons()->addAPerson($pipedrivePerson);
 
-                            if (! $addANoteResponse->success) {
-                                throw new \Exception("Could not create Pipedrive Note");
+                    if ($addAPersonResponse->success && isset($addAPersonResponse->data->id)) {
+                        $pipedrivePersonId = $addAPersonResponse->data->id;
+
+                        $pipedriveDeal = [
+                            'title' => $freeEstimateData['name']['value'],
+                            'person_id' => $pipedrivePersonId,
+                        ];
+
+                        $addADealResponse = $pipedriveClient->getDeals()->addADeal($pipedriveDeal);
+
+                        if ($addADealResponse->success && isset($addADealResponse->data->id)) {
+                            $pipedriveDealId = $addADealResponse->data->id;
+
+                            if (isset($freeEstimateData['comments']['value'])) {
+                                $pipedriveNote = [
+                                    'content' => $freeEstimateData['comments']['value'],
+                                    'dealId' => $pipedriveDealId,
+                                ];
+
+                                $addANoteResponse = $pipedriveClient->getNotes()->addANote($pipedriveNote);
+
+                                if (! $addANoteResponse->success) {
+                                    throw new \Exception("Could not create Pipedrive Note");
+                                }
                             }
-                        }
 
-                        if (isset($freeEstimateData['files'])) {
-                            if (function_exists('curl_file_create')) {
-                                foreach ($freeEstimateData['files'] as $file) {
-                                    if (isset($file['name'], $file['content'])) {
-                                        try {
-                                            $filepath = mkfile($file['content']);
+                            if (isset($freeEstimateData['files'])) {
+                                if (function_exists('curl_file_create')) {
+                                    foreach ($freeEstimateData['files'] as $file) {
+                                        if (isset($file['name'], $file['content'])) {
+                                            try {
+                                                $filepath = mkfile($file['content']);
 
-                                            $pipedriveFile = [
-                                                'file' => $filepath,
-                                                'dealId' => $pipedriveDealId,
-                                            ];
+                                                $pipedriveFile = [
+                                                    'file' => $filepath,
+                                                    'dealId' => $pipedriveDealId,
+                                                ];
 
-                                            $addFileResponse = $pipedriveClient->getFiles()->addFile($pipedriveFile);
-                                        } catch (\Throwable $e) {
-                                            \Sentry\captureException($e);
+                                                $addFileResponse = $pipedriveClient->getFiles()->addFile($pipedriveFile);
+                                            } catch (\Throwable $e) {
+                                                \Sentry\captureException($e);
+                                            }
+                                        } else {
+                                            \Sentry\captureMessage("Invalid file for Pipedrive: " . print_r($file, true));
                                         }
-                                    } else {
-                                        \Sentry\captureMessage("Invalid file for Pipedrive: " . print_r($file, true));
                                     }
                                 }
                             }
+                        } else {
+                            throw new \Exception("Could not create Pipedrive Deal");
                         }
                     } else {
-                        throw new \Exception("Could not create Pipedrive Deal");
+                        throw new \Exception("Could not create Pipedrive Person");
                     }
                 } else {
-                    throw new \Exception("Could not create Pipedrive Person");
+                    throw new \Exception("Setting 'pipedrive_api_token' not defined");
                 }
-            } else {
-                throw new \Exception("Setting 'pipedrive_api_token' not defined");
+            } catch (\Throwable $e) {
+                \Sentry\captureException($e);
             }
-        } catch (\Throwable $e) {
-            \Sentry\captureException($e);
+        } else {
+            throw new \Exception("Free Estimate Request with Invalid Google reCAPTCHA");
         }
+    } catch (\Throwable $e) {
+        \Sentry\captureException($e);
     }
 }
