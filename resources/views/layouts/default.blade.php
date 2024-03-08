24fade4179014c6bcd3316f832ec60b3bd5e39ef
Jonathan Roy <jroy66@gmail.com>
2024-03-08 07:30:45 -0500

    add google tag manager

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -35,6 +35,16 @@
             <!-- End cookieyes banner -->
         @endif
 
+        @if ($googleTagManagerId = $site->theme()->setting('google_tag_manager_id'))
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','{{ $googleTagManagerId }}');</script>
+            <!-- End Google Tag Manager -->
+        @endif
+
         @if ($googleAnalyticsId = $site->theme()->setting('google_analytics_id'))
             <!-- Google tag (gtag.js) -->
             <script async src="https://www.googletagmanager.com/gtag/js?id={{ $googleAnalyticsId }}"></script>
@@ -132,5 +142,12 @@
         <!-- Footer Code -->
         {!! $site->blocks('footer_code')->get()->render() !!}
         <!-- /Footer Code -->
+
+        @isset($googleTagManagerId)
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $googleTagManagerId }}"
+                              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+        @endisset
     </body>
 </html>
